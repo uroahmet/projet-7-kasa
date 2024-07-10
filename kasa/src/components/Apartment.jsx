51618a4302f9d8d74bfe97580eebb4f33ba2e6e4
uroahmet <ahmeturay35@gmail.com>
2024-07-10 17:42:22 +0200

    ajout du json

--- kasa/src/components/Apartment.jsx
+++ kasa/src/components/Apartment.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./Apartment.css"
 import  { Link } from "react-router-dom";
 
-function Apartment() {
+function Apartment(props) {
+  console.log("props", props);
   return (
     <Link to ="/flat">
     <div className='apartment'>
-        <div className='apartment__subtitle'>Titre de la location</div>
+    <img src={props.imageUrl} alt="" />
+        <div className='apartment__subtitle'>{props.title}</div>
         </div>
         </Link>
   )
--- kasa/src/components/ApartmentGrid.jsx
+++ kasa/src/components/ApartmentGrid.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ApartmentGrid.css"
 import Apartment from "./Apartment.jsx"
 
 function ApartmentGrid() {
+    const [apartments, setApartments] = useState([]);
+
+  useEffect(fetchApartments, []);
+
+  function fetchApartments() {
+  fetch("logements.json")
+  .then ((res) => res.json ())
+  .then ((res) => setApartments(res))
+  }
+
   return (
     <div className="grid">
-        <Apartment />
-        <Apartment />
-        <Apartment />
-        <Apartment />
+        {apartments.map((apartment) => (
+          <Apartment title={apartment.title} imageUrl={apartment.cover} />
+        ))}
         </div>
   )
 }
-
 export default ApartmentGrid
