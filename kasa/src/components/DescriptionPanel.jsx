0eaba4e6c51bfb650c568f8bc2c9181f17a24dfe
uroahmet <ahmeturay35@gmail.com>
2024-07-12 18:51:00 +0200

    few changes

--- kasa/src/components/DescriptionPanel.jsx
+++ kasa/src/components/DescriptionPanel.jsx
@@ -11,13 +11,25 @@ export function DescriptionPanel(props) {
   const contentClass = (isContentVisible ? "visible" : "hidden") + " description__content";
   const chevronClass = (isContentVisible ? "rotate-up" : "rotate-down") + " fas fa-chevron-down";
 
+  let content;
+  if (typeof props.content === "string") {
+    content = <p className={contentClass}>{props.content}</p>;
+  } else {
+
+    content = (
+      <div className={contentClass}>
+        {props.content}
+      </div>
+    );
+  }
+
   return (
     <div className="description__panel">
       <p className="description__header" onClick={showContent}>
         <span>{props.title}</span>
         <i className={chevronClass}></i>
       </p>
-      <p className={contentClass}>{props.content}</p>
+      {content}
     </div>
   );
 }
